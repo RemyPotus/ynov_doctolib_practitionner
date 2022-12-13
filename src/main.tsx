3397ddafebc107d5bc528f46c1cfd78837d0c33e
Rémy Potus <remy.potus@gmail.com>
2022-12-13 15:04:34 +0100

    feat: configure firebase for global usage

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,33 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import Root from "./routes/root";
 import Authentication from './pages/authentication';
 import ErrorPage from './pages/error-page';
 import MyAppointments from './pages/myAppointments';
 import './index.css'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain:import.meta.env.VITE_AUTH_DOMAIN,
+  storageBucket:import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGING_SENDER_ID,
+  projectId:import.meta.env.VITE_PROJECT_ID,
+  appId:import.meta.env.VITE_APP_ID,
+  reactAppId: import.meta.env.VITE_VITE_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const db = getFirestore(app);
+
+// Initialize Firestore - Auth
+export const auth = getAuth();
 
 const router = createBrowserRouter([
   {
