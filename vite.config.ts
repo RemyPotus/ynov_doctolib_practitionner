46ebb8b7a6f1252f7bf372d9227ad0af6e0f10d5
Rémy Potus <remy.potus@gmail.com>
2022-12-13 14:52:15 +0100

    feat: init vite / PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate', workbox: { clientsClaim: true, skipWaiting: true }, devOptions: { enabled: true, }, includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'], manifest: { name: 'Simon game', short_name: 'Simon', description: 'Like my app', theme_color: '#ffffff', icons: [ { src: 'android-chrome-192x192.png', sizes: '192x192', type: 'image/png' }, { src: 'android-chrome-512x512.png', sizes: '512x512', type: 'image/png' } ] } })
+  ],
 })
