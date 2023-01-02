eea8d51fc09368c07a8a78ae3aedd2fe71da987e
Rémy Potus <remy.potus@gmail.com>
2023-01-02 11:05:22 +0100

    feat: create install button

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import './root.css';
 import listIcon from '../assets/list-icon.png';
+import ButtonInstallPWA from '../components/ButtonInstallPWA';
 
 export default function Root() {
     return (
@@ -14,6 +15,7 @@ export default function Root() {
                         <div className="navbarItem">
                             <a className="iconCentered" href={`my-appointments`}><img className="navbarIcon" src={listIcon} alt="my appointments"/></a>
                         </div>
+                        {<ButtonInstallPWA/>}
                     </nav>
                 </div>
             </div>
