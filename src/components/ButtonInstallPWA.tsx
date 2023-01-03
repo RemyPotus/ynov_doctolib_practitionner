6b5a7c2c8b9d46ccf62a2c9ba416d47974d4aaf5
Rémy Potus <remy.potus@gmail.com>
2023-01-03 14:28:16 +0100

    fix: new install button

--- src/components/ButtonInstallPWA.tsx
+++ src/components/ButtonInstallPWA.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import installIcon from '../assets/install-icon.png'
 
 const ButtonInstallPWA = () => {
+  /*
   const [supportsPWA, setSupportsPWA] = useState(false);
   const [promptInstall, setPromptInstall]= useState<any>(null);
 
   useEffect(() => {
     const handler = (e:any) => {
+      console.log('test');
       e.preventDefault();
       setSupportsPWA(true);
       setPromptInstall(e);
@@ -23,12 +25,33 @@ const ButtonInstallPWA = () => {
     }
     promptInstall.prompt();
   };
+  */
+
+  useEffect(() => {
+    let deferredPrompt:any;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+    });
+
+    const installApp = document.getElementById('installApp');
+    installApp?.addEventListener('click', async () => {
+      if (deferredPrompt !== null) {
+          deferredPrompt.prompt();
+          const { outcome } = await deferredPrompt.userChoice;
+          if (outcome === 'accepted') {
+              deferredPrompt = null;
+          }
+      }
+    });
+  }, [])
+
+
   return (
     <>
     {
-      supportsPWA && (
+      /*supportsPWA && onClick={onClick}*/ (
         <div className="navbarItem">
-          <a className="iconCentered"><img className="navbarIcon" src={installIcon} alt="install app" onClick={onClick} /></a>
+          <a className="iconCentered"><img id="installApp" className="navbarIcon" src={installIcon} alt="install app" /></a>
         </div>
       )
     }
